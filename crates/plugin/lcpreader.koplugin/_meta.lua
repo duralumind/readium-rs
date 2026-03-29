002edce5ad522fda49d36f6ce8fc2dcf54fbd983
Pawan Dhananjay <pawandhananjay@gmail.com>
2026-03-29 15:27:02 -0700

    KOReader plugin lua file

---
+++ crates/plugin/lcpreader.koplugin/_meta.lua
@@ -0,0 +1,6 @@
+local _ = require("gettext")
+return {
+    name = "lcpreader",
+    fullname = _("LCP Reader"),
+    description = _([[Decrypt and read Readium LCP protected EPUBs. Supports the LCP Basic Profile encryption.]]),
+}
